35d3a720c6b9587d9c80e33eb7cf3b768d39401d
carlos <yicarlos@qq.com>
2024-11-20 22:06:01 +0800

    new update

--- pytorch_utils.h
+++
@@ -1,8 +0,0 @@
-// #ifndef PYTORCH_UTILS_H
-// #define PYTORCH_UTILS_H
-
-// #include <torch/torch.h>
-
-// torch::jit::script::Module load_model(const std::string& model_path);
-
-// #endif // PYTORCH_UTILS_H
